496e636b0e7cac78765edfe710159965109cfc85
Roger Gerzner <rogergerzner@hispeed.ch>
2024-11-29 19:42:13 +0100

    Erweiterung mit Glückssymbol und Glückszahl

--- ThemaGit/OrakelVonBaden/src/main/kotlin/Orakel.kt
+++ ThemaGit/OrakelVonBaden/src/main/kotlin/Orakel.kt
@@ -9,9 +9,31 @@ import kotlin.random.Random
  */
 
 // Die Sprüche des Tages
-val spells = listOf("Nutze den Tag.", "Heute ist ein toller Tag.")
+val spells = listOf(
+    "Nutze den Tag.",
+    "Heute ist ein toller Tag.",
+    "Glaube an dich selbst.",
+    "Jeder Tag ist ein neuer Anfang.",
+    "Das Leben ist schön.",
+    "Folge deinem Herzen.",
+    "Sei mutig und stark.",
+    "Lächle und die Welt lächelt zurück."
+)
 
 // Todo: Hier die Liste mit den Glückssymbolen einfügen
+
+val luckySymbols = listOf(
+    "Kleeblatt",
+    "Hufeisen",
+    "Marienkäfer",
+    "Glückspfennig",
+    "Schornsteinfeger",
+    "Glücksschwein",
+    "Regenbogen",
+    "Sternschnuppe"
+)
+
+
 
 fun main() {
     val random = Random
@@ -19,6 +41,12 @@ fun main() {
     println("Dein Leitspruch heute: ${spells[index]}")
 
     // Todo: Hier das Glückssymbol ausgeben
+    val index2 = random.nextInt(luckySymbols.size)
+    println("Dein Glückssymbol heute: ${luckySymbols[index2]}")
+
 
     // Todo: Hier die Glückszahl berechnen und ausgeben
+    val luckyNumber = random.nextInt(1, 100)
+    println("Deine Glückszahl heute: $luckyNumber")
+
 }
